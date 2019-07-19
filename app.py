1baaac7ed92f247f4b6b2e5c6fec7a61f4d1d1c5
Matt <mwcooley99@gmail.com>
2019-07-19 12:08:44 -0700

    added basic scheduler for testing

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+
+
+def tick():
+    print(f'The time right now is: {datetime.now()}')
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(tick, 'interval', seconds=10)
+scheduler.start()
 
 
 @app.route('/')
