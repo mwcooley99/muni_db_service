25cc8b3b4eef10687505d3528b854a042be388a7
Matt <mwcooley99@gmail.com>
2019-07-23 21:18:11 -0700

    updated the model

--- models.py
+++ models.py
@@ -7,11 +7,14 @@ class Prediction(db.Model):
                    autoincrement=True)
     response_time = db.Column(db.DateTime)
     recorded_time = db.Column(db.DateTime)
-    line_ref = db.Column(db.String)
-    direction_ref = db.Column(db.String)
-    stop_point_ref = db.Column(db.Integer)
+    line_ref = db.Column(db.String, index=True)
+    direction_ref = db.Column(db.String, index=True)
+    stop_point_ref = db.Column(db.Integer, index=True)
     scheduled_arrival_time = db.Column(db.DateTime)
     expected_arrival_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return f'<Stop: {self.stop_point_ref} Line: {self.line_ref}>'
+
+
+
